649294de208b3ff23c50cf7d5915c07b5752c7ff
fre2157476 <151277865+fre2157476@users.noreply.github.com>
2026-04-21 00:45:33 -0400

    Delete capstone-proposal-s26 directory

--- capstone-proposal-s26/src/test_connection.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import text
-from database.connection import engine
-
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT current_database();"))
-    for row in result:
-        print("Connected to:", row[0])
